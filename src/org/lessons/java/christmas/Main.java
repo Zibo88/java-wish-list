f2d76c4829d188fd82b1192997176bf9f31af83e
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-02-02 15:03:28 +0100

    correzione indentazone e aggiunta data.close()

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -10,43 +10,47 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+//		creo la lista
 		ArrayList<String> listaDesideri = new ArrayList<String>();
+//		creo una variabile che rappresenta la scelta fatta dall'utente
 		String userChoiche;
+//		utilizzo la classe Scanner per leggere i dati dlel'utente
 		Scanner data = new Scanner(System.in);
 		
 		
-			System.out.println("inserisci un nuovo desiderio");
-			userChoiche = data.nextLine();
-			listaDesideri.add(userChoiche);
-			int lunghezzaLista=0;
+		System.out.println("inserisci un nuovo desiderio");
+		userChoiche = data.nextLine();
+		listaDesideri.add(userChoiche);
+		int lunghezzaLista=0;
 			
-			while(!userChoiche.equals("")){
-				System.out.println("Aggiungi un altro elemento o premi enter per terminare l'operazione");
-				userChoiche = data.nextLine();
-				listaDesideri.add(userChoiche);
-				if(userChoiche.equals("")) {
-					System.out.println("Hai deciso di terminare");
-				}
+		while(!userChoiche.equals("")){
+		System.out.println("Aggiungi un altro elemento o premi enter per terminare l'operazione");
+		userChoiche = data.nextLine();
+		listaDesideri.add(userChoiche);
+		if(userChoiche.equals("")) {
+			System.out.println("Hai deciso di terminare");
+			data.close();
 			}
-				
-			System.out.println("La tua lista è composta da:");
-			for(int i=0; i < listaDesideri.size()-1; i++) {
-				lunghezzaLista = listaDesideri.size()-1;
-			}
-			System.out.println(lunghezzaLista + " " + "elementi");
+		}
 			
-//			Ordinare in ordine alfabetico
-			Collections.sort(listaDesideri, Comparator.naturalOrder());
-			System.out.println(listaDesideri);
+		System.out.println("La tua lista è composta da:");
+		for(int i=0; i < listaDesideri.size()-1; i++) {
+			lunghezzaLista = listaDesideri.size()-1;
+		}
+		System.out.println(lunghezzaLista + " " + "elementi");
 			
-			String[] arrayList = new String[ listaDesideri.size()];
-			listaDesideri.toArray(arrayList);
+//		Ordinare in ordine alfabetico
+		Collections.sort(listaDesideri, Comparator.naturalOrder());
+		System.out.println(listaDesideri);
 			
-			for(int i = 0; i < arrayList.length;i++) {
-				if(!arrayList[i].equals(""))
+		String[] arrayList = new String[ listaDesideri.size()];
+		listaDesideri.toArray(arrayList);
+			
+		for(int i = 0; i < arrayList.length;i++) {
+			if(!arrayList[i].equals(""))
 					
-				System.out.println("il tuo desiderio" + " " + arrayList[i] + " " + "si trova in posizione" + i);
-			}
+			System.out.println("il tuo desiderio" + " " + arrayList[i] + " " + "si trova in posizione" + i);
+		}
 	}
 
 }
